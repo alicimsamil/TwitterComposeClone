cab9da8a4735e50f6f653d17efc5a12617aef2f8
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-08-30 02:42:42 +0300

    Tweet content setted.

--- app/src/main/java/com/alicimsamil/twittercomposeclone/TweetRow.kt
+++ app/src/main/java/com/alicimsamil/twittercomposeclone/TweetRow.kt
@@ -2,18 +2,19 @@ package com.alicimsamil.twittercomposeclone
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alicimsamil.twittercomposeclone.ui.theme.TwitterBlack
+import com.alicimsamil.twittercomposeclone.ui.theme.TwitterDarkGray
 import com.alicimsamil.twittercomposeclone.ui.theme.TwitterWhite
 
 class TweetRow {
@@ -22,7 +23,10 @@ class TweetRow {
     @Composable
     fun TwitterTweetRow(){
 
-        Row(modifier = Modifier.fillMaxSize().background(TwitterWhite).padding(10.dp)) {
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .background(TwitterWhite)
+            .padding(10.dp)) {
 
             Image(
                 painter = painterResource(id = R.drawable.profilepic),
@@ -31,6 +35,45 @@ class TweetRow {
                     .clip(CircleShape)
                     .size(55.dp)
             )
+            
+            
+            Spacer(modifier = Modifier.size(10.dp))
+            
+            
+            Column() {
+                
+                Row() {
+                    
+                    Text(text = "Ali Şamil", fontWeight = FontWeight.Bold, fontSize = 14.sp, color = TwitterBlack)
+                    
+                    Spacer(modifier = Modifier.size(5.dp))
+                    
+                    Text(text = "@Alicimsamil", fontSize = 14.sp, color = TwitterDarkGray)
+
+                    Spacer(modifier = Modifier.size(5.dp))
+
+                    Text(text = "·", fontSize = 14.sp, color = TwitterDarkGray)
+
+                    Spacer(modifier = Modifier.size(5.dp))
+                    
+                    Text(text = "11h", fontSize = 14.sp, color = TwitterDarkGray)
+                    
+                    
+                }
+                
+                Row {
+                    
+                    Text(
+                        text = "Sene 2021 adam çıkıyor 30 liralık eğitime bok atıp 10 euroluk eğitim satıyor. Büyük bir platformda neden eğitim vermiyorsunuz diye soruyolar çünkü soru soramıyosun efektif olmuyo diyor ama ama diyor işte biz bir yerde kurs verdik bunların da videoları duruyor isterseniz...",
+                        fontSize = 14.sp,
+                        color = TwitterBlack
+                    )
+                    
+                    
+                }
+                
+                
+            }
 
 
 
