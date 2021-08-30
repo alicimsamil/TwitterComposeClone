aace4de5ae0393b51414059c9b4639b13492835c
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-08-30 03:36:05 +0300

    Tweet structure completed and added to scaffold with lazy column also tweet model created.

--- app/src/main/java/com/alicimsamil/twittercomposeclone/TweetRow.kt
+++ app/src/main/java/com/alicimsamil/twittercomposeclone/TweetRow.kt
@@ -6,27 +6,37 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.alicimsamil.twittercomposeclone.model.TweetContent
 import com.alicimsamil.twittercomposeclone.ui.theme.TwitterBlack
 import com.alicimsamil.twittercomposeclone.ui.theme.TwitterDarkGray
 import com.alicimsamil.twittercomposeclone.ui.theme.TwitterWhite
 
 class TweetRow {
 
-    @Preview
     @Composable
-    fun TwitterTweetRow(){
+    fun TwitterTweetRow(tweetContent: TweetContent){
 
-        Row(modifier = Modifier
-            .fillMaxSize()
-            .background(TwitterWhite)
-            .padding(10.dp)) {
+        Row(tweetContent)
+
+
+    }
+
+
+    @Composable
+    fun Row(tweetContent: TweetContent){
+
+
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
 
             Image(
                 painter = painterResource(id = R.drawable.profilepic),
@@ -35,44 +45,95 @@ class TweetRow {
                     .clip(CircleShape)
                     .size(55.dp)
             )
-            
-            
+
+
             Spacer(modifier = Modifier.size(10.dp))
-            
-            
-            Column() {
-                
-                Row() {
-                    
-                    Text(text = "Ali Şamil", fontWeight = FontWeight.Bold, fontSize = 14.sp, color = TwitterBlack)
-                    
+
+
+            Column {
+
+                Row {
+
+                    Text(text = "${tweetContent.name}", fontWeight = FontWeight.Bold, fontSize = 14.sp, color = TwitterBlack)
+
                     Spacer(modifier = Modifier.size(5.dp))
-                    
-                    Text(text = "@Alicimsamil", fontSize = 14.sp, color = TwitterDarkGray)
+
+                    Text(text = "${tweetContent.userName}", fontSize = 14.sp, color = TwitterDarkGray)
 
                     Spacer(modifier = Modifier.size(5.dp))
 
                     Text(text = "·", fontSize = 14.sp, color = TwitterDarkGray)
 
                     Spacer(modifier = Modifier.size(5.dp))
-                    
+
                     Text(text = "11h", fontSize = 14.sp, color = TwitterDarkGray)
-                    
-                    
+
+
                 }
-                
+
                 Row {
-                    
+
                     Text(
-                        text = "Sene 2021 adam çıkıyor 30 liralık eğitime bok atıp 10 euroluk eğitim satıyor. Büyük bir platformda neden eğitim vermiyorsunuz diye soruyolar çünkü soru soramıyosun efektif olmuyo diyor ama ama diyor işte biz bir yerde kurs verdik bunların da videoları duruyor isterseniz...",
+                        text = "${tweetContent.content}",
                         fontSize = 14.sp,
                         color = TwitterBlack
                     )
-                    
-                    
+
+
                 }
-                
-                
+
+                Spacer(modifier = Modifier.size(5.dp))
+
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.twittercomment),
+                        contentDescription = "Comment Button",
+                        Modifier.size(25.dp)
+                    )
+
+                    Spacer(modifier = Modifier.size(5.dp))
+
+                    Text(text = "40", color = TwitterDarkGray)
+
+                    Spacer(modifier = Modifier.size(30.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.twitterretweet),
+                        contentDescription = "Retweet Button",
+                        Modifier.size(25.dp)
+                    )
+
+                    Spacer(modifier = Modifier.size(5.dp))
+
+                    Text(text = "1,069", color = TwitterDarkGray)
+
+                    Spacer(modifier = Modifier.size(30.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.twitterlike),
+                        contentDescription = "Like Button",
+                        Modifier.size(22.dp)
+                    )
+
+                    Spacer(modifier = Modifier.size(5.dp))
+
+                    Text(text = "16.3K", color = TwitterDarkGray)
+
+                    Spacer(modifier = Modifier.size(20.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.twittershare),
+                        contentDescription = "Share Button",
+                        Modifier.size(25.dp)
+                    )
+
+
+                }
+
+
+
             }
 
 
@@ -91,10 +152,12 @@ class TweetRow {
 
 
 
+
+
+
+
+
     }
-
-
-
 
 
 
---
+++ app/src/main/java/com/alicimsamil/twittercomposeclone/model/TweetContent.kt
@@ -0,0 +1,7 @@
+package com.alicimsamil.twittercomposeclone.model
+
+data class TweetContent(
+    val name : String,
+    val userName : String,
+    val content : String
+    )
